d5c4ee82aacf6cc3ad26c082d5641b510a958e33
aditya <technical28.multiicon@gmail.com>
2026-05-25 14:22:20 +0530

    feat: initialize Django project structure and add strategy engine configuration files

---
+++ mm_project/settings.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+from config import Config
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = Config.SECRET_KEY
+
+DEBUG = Config.DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+    'chat',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'mm_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mm_project.wsgi.application'
+
+DATABASES = {}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
